7174092069c36ff32980c46ddeca3698aa44efcf
Evgeny Chausov <evgeny.chausov@kreo.net>
2025-09-28 13:52:44 +0300

    fix linter

--- accounting/vite.config.ts
+++ accounting/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/accounting/",
+  base: '/accounting/',
   css: {
     preprocessorOptions: {
       scss: {
